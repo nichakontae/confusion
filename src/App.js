905f94097c4b5c95fda939db2ecdabda466850dc
Nichakon <hell-.eve555@gmail.com>
2022-05-09 13:49:09 +0700

    W1-01 Configuring React

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Navbar, NavbarBrand } from "reactstrap";
+import "./App.css";
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default App;
